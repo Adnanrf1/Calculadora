f0774b63c74f21ffa41713289ac4d4c0083a7d9f
Mateus <mateus-vinicius19@live.com>
2019-04-29 11:52:44 -0300

    Primeiro envio

---
+++ Calculadora.py
@@ -0,0 +1,34 @@
+#def binaoct(a):
+    #lista1 = [000, 001, 010, 011, 100, 101, 110, 111]
+   #lista2 = [0, 1, 2, 3, 4, 5, 6, 7]
+    #lista3 =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+    #lista4 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F]
+    
+
+
+
+print("******************************************")
+print("*************** CALCULADORA **************")
+print("******************************************")
+print("** Escolha a opção para calcular      ****")
+print("***[1] binario                        ****")
+print("***[2] Octal                          ****")
+print("***[3] Decimal                        ****")
+print("***[4] Hexadecimal                    ****")
+print("***[0] SAIR                           ****")
+print("******************************************")
+n1 = int(input("Escolha uma opção: "))
+
+if n1 == 1:
+    print("******************************************")
+    print("***[2] Octal                          ****")
+    print("***[3] Decimal                        ****")
+    print("***[4] Hexadecimal                    ****")
+    print("******************************************")
+    n2 = int = input("Para qual você quer converter?: ")
+    if n2==2:
+        n3= int(input("Qual o número?: "))
+        binaoct(n3)
+
+
+
